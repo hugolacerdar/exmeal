e1559295ea5b4f23383879aee3941778ff4fc9cb
Hugo <hugolacerdar@gmail.com>
2021-05-24 13:20:09 -0300

    Adding error struct

---
+++ lib/exmeal/error.ex
@@ -0,0 +1,16 @@
+defmodule Exmeal.Error do
+  @keys [:status, :result]
+
+  @enforce_keys @keys
+
+  defstruct @keys
+
+  def build(status, result) do
+    %__MODULE__{
+      status: status,
+      result: result
+    }
+  end
+
+  def meal_not_found, do: build(:not_found, "Meal not found")
+end
